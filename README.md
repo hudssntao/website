ea45ad1eba368e51a20eeb68b345430585a0dff5
hudssntao <hudsontao@gmail.com>
2024-06-15 12:37:02 -0400

    delete react app

--- README.md
+++
@@ -1,1 +0,0 @@
-# hudssntao.github.io
--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="text-center p-6">
-      <h1 className="text-3xl font-bold underline">Hello, Tailwind CSS!</h1>
-    </div>
-  );
-}
